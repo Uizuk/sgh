8b559f728841267511f91cb85c0b4542850b80a3
Uizuk <uizuk94@gmail.com>
2019-12-04 13:16:54 +0100

    update repo contents

--- pracka2.py
+++ pracka2.py
@@ -4,14 +4,14 @@ f = "elo.txt"
 file = open(f, "r")
 
 for line in file.read().split():
-    line = line.strip().lower()
-    line = re.sub(r'[^\w\s+]',' ',line)
+    line = line.lower().strip()
+    line = re.sub(r'[^A-Za-z0-9]+','',line)
     if not(line in d):
         d[line] = 1
     else:
         num = d[line] + 1
         d.update({line: num})
 
-for key, value in sorted(d.items(), key=lambda d: d[1], reverse=True):
+for key, value in sorted(d.items(), key=lambda d: d[1], reverse=False): #Swap to True, to reverse order
     print(f"\nSłowo: {key}")
     print(f"Liczba wystąpień: {value}")
